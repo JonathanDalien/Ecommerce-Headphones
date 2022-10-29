ad67799c7efd6d1565dc6a38f40045e34a8bf7bd
LegendeJJ <dalienjj@gmail.com>
2022-10-29 10:50:28 +0200

    Added login page

---
+++ pages/login.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { useForm, Controller } from "react-hook-form"
+import { TextField } from '@mui/material';
+
+const login = () => {
+
+    const { handleSubmit, control, formState: { errors } } = useForm();
+
+    const submitHanlder = async (email, password) => {
+
+    }
+
+    return (
+        <div className='login-container'>
+            <h1>Login</h1>
+            <form className='login-form' onSubmit={handleSubmit(submitHanlder)}>
+                <Controller name='email' control={control} defaultValue="" rules={{ required: true }} render={({ field }) => (
+                    <TextField variant='outlined' fullWidth id='email' label="Email" inputProps={{ type: "email" }}
+                        error={Boolean(errors.email)}
+                        helperText={errors.email ? errors.email.type === "pattern" ? "Email is not valid" : "Email is required" : ""}>
+                    </TextField>
+                )}></Controller>
+                <Controller name='password' control={control} defaultValue="" rules={{ required: true }} render={({ field }) => (
+                    <TextField variant='outlined' fullWidth id='password' label="Passwort" inputProps={{ type: "email" }}
+                        error={Boolean(errors.email)}
+                        helperText={errors.email ? errors.email.type === "pattern" ? "Email is not valid" : "Email is required" : ""}>
+                    </TextField>
+                )}></Controller>
+                <button className='btn' type='submit'>Anmelden</button>
+            </form>
+        </div>
+    )
+}
+
+export default login
