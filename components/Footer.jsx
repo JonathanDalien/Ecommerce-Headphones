1a26a1271f34e41ae2ac619440188024b21c7544
LegendeJJ <dalienjj@gmail.com>
2022-11-02 14:56:07 +0100

    edited footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,7 +5,7 @@ import { AiFillGithub } from 'react-icons/ai'
 const Footer = () => {
     return (
         <div className='footer-container'>
-            <p>2022 Jonathan Dalien All rights reserved</p>
+            <p>2022 Jonathan Dalien - All rights reserved</p>
             <p className='icons'>
                 <Link className='github-button' href="https://github.com/JonathanDalien/Ecommerce-Headphones">
                     <AiFillGithub />
